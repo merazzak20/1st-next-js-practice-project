f1c76ac657cfea065a7cfc81afb8623e99cd89db
merazzak <info.merazzak@gmail.com>
2025-01-24 12:03:35 +0600

    update link

--- src/Blogs/Blogs.jsx
+++ src/Blogs/Blogs.jsx
@@ -22,7 +22,7 @@ const Blogs = () => {
   }, []);
   return (
     <div className="my-7 container mx-auto px-10">
-      <h2 className="text-3xl font-bold">Available Blogs: {blogs.length}</h2>
+      <h2 className="text-xl font-semibold">Available Blogs: {blogs.length}</h2>
       <div className="mt-10">
         {blogs.map((blog, idx) => (
           <SingleBlog key={blog.id} index={idx} blog={blog}></SingleBlog>
--- src/app/page.js
+++ src/app/page.js
@@ -2,7 +2,7 @@ import Blogs from "../Blogs/Blogs";
 
 export default function Home() {
   return (
-    <main className="flex flex-col min-h-screen text-center">
+    <main className="flex flex-col min-h-screen text-center px-10 py-10">
       <>
         <Blogs></Blogs>
       </>
